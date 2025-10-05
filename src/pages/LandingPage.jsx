abdcce68ec4ac40074eee039465b3bf2238ac8bb
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-05 18:17:44 +0530

    Add Projects component to LandingPage for improved project visibility

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from '../components/Home'
 import About from '../components/About'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 const LandingPage = () => {
   return (
     <>
         <Home/>
         <About/>
         <Skills/>
+        <Projects/>
     </>
   )
 }
