6aaddfbf3be573fcfe7d1a5236cd5f91a2a48164
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-05 16:42:07 +0530

    Add Skills component to LandingPage for enhanced content display

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from '../components/Home'
 import About from '../components/About'
+import Skills from '../components/Skills'
 const LandingPage = () => {
   return (
     <>
         <Home/>
         <About/>
+        <Skills/>
     </>
   )
 }
