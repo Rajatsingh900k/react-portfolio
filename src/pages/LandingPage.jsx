19f2c3884a42f1c7fb1410a7900b327a63f33897
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-05 15:54:28 +0530

    Add LandingPage component to render all the sections

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from '../components/Home'
+import About from '../components/About'
+const LandingPage = () => {
+  return (
+    <>
+        <Home/>
+        <About/>
+    </>
+  )
+}
+
+export default LandingPage
