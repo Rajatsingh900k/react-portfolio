b83dfab8d213db328d6614bdcaabe6a91621bef0
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-05 15:54:59 +0530

    Refactor App component to include Navbar and routing for LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
-
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import About from './components/About'
+import LandingPage from './pages/LandingPage'
 function App() {
-
   return (
     <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+      </Routes>
     </>
   )
 }
